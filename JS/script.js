20603d53539985099ee0e46203724edfa6462dcf
Umidjon8787 <100425651+Umidjon8787@users.noreply.github.com>
2022-03-30 17:42:53 +0300

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,125 @@
+let kidswear = 0
+let buy = document.getElementById("buy")
+
+
+
+function qushish() {
+    
+    kidswear = kidswear + 1
+    buy.innerText = kidswear
+}
+
+function ayirish() {
+    kidswear = kidswear - 1
+    buy.innerText = kidswear
+}
+
+
+let kuylak = 0
+let dress = document.getElementById("dress")
+
+
+
+function qush() {
+    
+    kuylak = kuylak + 1
+    dress.innerText = kuylak
+}
+
+function ayir() {
+    kuylak = kuylak - 1
+    dress.innerText = kuylak
+}
+
+
+let kiyim = 0
+let komplekt = document.getElementById("komplekt")
+
+function plusp() {
+    kiyim = kiyim + 1
+    komplekt.innerText = kiyim
+}
+
+function minusp() {
+    kiyim = kiyim - 1
+    komplekt.innerText = kiyim
+}
+
+
+let kiyimcha = 0
+let bear = document.getElementById("bear")
+
+function plus() {
+    kiyimcha = kiyimcha + 1
+    bear.innerText = kiyimcha
+}
+
+function minus() {
+    kiyimcha = kiyimcha - 1
+    bear.innerText = kiyimcha
+}
+
+let kiyimchaa = 0
+let fudbolka = document.getElementById("fudbolka")
+
+function pluss() {
+    kiyimchaa = kiyimchaa + 1
+    fudbolka.innerText = kiyimchaa
+}
+
+function minuss() {
+    kiyimchaa = kiyimchaa - 1
+    fudbolka.innerText = kiyimchaa
+}
+
+let neki = 0
+let nike = document.getElementById("nike")
+
+function plussa() {
+    neki = neki + 1
+    nike.innerText = neki
+}
+
+function minussa() {
+    neki = neki - 1
+    nike.innerText = neki
+}
+
+let neki1 = 0
+let nike1 = document.getElementById("nike1")
+
+function plussa1() {
+    neki1 = neki1 + 1
+    nike1.innerText = neki1
+}
+
+function minussa1() {
+    neki1 = neki1 - 1
+    nike1.innerText = neki1
+}
+
+let neki3 = 0
+let nike3 = document.getElementById("nike3")
+
+function plussa3() {
+    neki3 = neki3 + 1
+    nike3.innerText = neki3
+}
+
+function minussa3() {
+    neki3 = neki3 - 1
+    nike3.innerText = neki3
+}
+
+let neki2 = 0
+let nike2 = document.getElementById("nike2")
+
+function plussa2() {
+    neki2 = neki2 + 1
+    nike2.innerText = neki2
+}
+
+function minussa2() {
+    neki2 = neki2 - 1
+    nike2.innerText = neki2
+}
